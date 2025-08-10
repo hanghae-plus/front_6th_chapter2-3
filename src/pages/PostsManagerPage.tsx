c72a71b100570bb5615000c4b8496e9f7439f066
YeongseoYoon <l112xpla@gmail.com>
2025-08-11 00:57:14 +0900

    feat: 게시물 삭제 기능을 react-query의 뮤테이션으로 변경 - 기존 fetch 방식에서 뮤테이션을 사용하여 게시물 삭제 로직 개선

--- src/pages/PostsManagerPage.tsx
+++ src/pages/PostsManagerPage.tsx
@@ -104,16 +104,9 @@ const PostsManager = () => {
     )
   }
 
-  // 게시물 삭제
-  const deletePost = async (id: number) => {
-    try {
-      await fetch(`/api/posts/${id}`, {
-        method: "DELETE",
-      })
-      setPosts((posts as any).filter((post: any) => post.id !== id))
-    } catch (error) {
-      console.error("게시물 삭제 오류:", error)
-    }
+  const deletePostMutation = useMutation(postMutations.deleteMutation())
+  const deletePost = (id: number) => {
+    deletePostMutation.mutate(id)
   }
 
   // 댓글 가져오기
