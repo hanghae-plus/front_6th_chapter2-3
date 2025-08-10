b69fa7831651064a2416b91a0713584f21d674d4
Hwirin-Kim <kimhuirin@gmail.com>
2025-08-10 19:50:54 +0900

    fix : 공통ui type정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router } from "react-router-dom"
-import Header from "./components/Header.tsx"
-import Footer from "./components/Footer.tsx"
+import Header from "./shared/ui/header/Header.tsx"
+import Footer from "./shared/ui/footer/Footer.tsx"
 import PostsManagerPage from "./pages/PostsManagerPage.tsx"
 
 const App = () => {
