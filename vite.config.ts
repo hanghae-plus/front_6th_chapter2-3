40cd08d6196901fbacc593ef4885470f3a3244dc
kimsh <kimsh@laonroad.com>
2025-08-11 15:38:04 +0900

    chore: 배포 & lint 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/front_6th_chapter2-3/',
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
+      '/api': {
         // target: 'https://jsonplaceholder.typicode.com',
-        target: "https://dummyjson.com",
+        target: 'https://dummyjson.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
-})
+});
