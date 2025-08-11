c65cf496d1c5d360c8c38696445d179b3e5bce71
BBAK_JUN <wnsguddl789@gmail.com>
2025-08-11 21:07:06 +0900

    feat(shared): add utility hooks and services

---
+++ src/shared/lib/index.ts
@@ -0,0 +1,2 @@
+export * from "./http-client"
+export * from "./query-client"
---
+++ src/shared/types/index.ts
@@ -0,0 +1,6 @@
+import { ComponentProps, ElementType } from "react"
+
+export type PolymorphicProp<E extends ElementType, T> = Omit<ComponentProps<E>, keyof T> & {
+  as?: E
+  asChild?: boolean
+} & T
