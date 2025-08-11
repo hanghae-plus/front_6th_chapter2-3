441003f9e75b37d7dfcf7d1509cf411a655ac25a
eveneul <nebulas9724@gmail.com>
2025-08-12 02:30:38 +0900

    refactor: posts 및 user API 경로 수정

--- src/features/posts/list-posts/api.ts
+++ src/features/posts/list-posts/api.ts
@@ -1,7 +1,7 @@
-import { fetchPosts } from '../../../entities/post/model/api';
-import { Posts } from '../../../entities/post/model/type';
-import { fetchUserBasic } from '../../../entities/user/model/api';
-import { UserBasic } from '../../../entities/user/model/type';
+import { fetchPosts } from '../../../entities/post/api/api';
+import { Posts } from '../../../entities/post/types';
+import { fetchUserBasic } from '../../../entities/user/api/api';
+import { UserBasic } from '../../../entities/user/types';
 
 export async function fetchPostsWithAuthors(limit: number, skip: string) {
   const [postsData, usersData] = await Promise.all([fetchPosts(limit, skip), fetchUserBasic()]);
