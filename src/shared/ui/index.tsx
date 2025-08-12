ac624a9e40016cb64eae241280eaaa0fb2abbae0
JiHoon-0330 <ljihoon0330@gmail.com>
2025-08-12 23:07:30 +0900

    UI 컴포넌트 분리

---
+++ src/shared/ui/index.tsx
@@ -0,0 +1,14 @@
+export { Button } from './Button';
+export { Input } from './Input';
+export { Card, CardContent, CardHeader, CardTitle } from './Card';
+export { Textarea } from './Textarea';
+export { SelectContent, SelectItem, SelectTrigger } from './Select';
+export { DialogContent, DialogHeader, DialogTitle } from './Dialog';
+export {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from './Table';
