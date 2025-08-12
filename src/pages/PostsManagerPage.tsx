5c88f8f96901017759f2cdb21ee0b72b61320037
YeongseoYoon <l112xpla@gmail.com>
2025-08-13 01:18:08 +0900

    feat: 사용자 프로필 컴포넌트 추가 - 사용자 정보를 표시하는 UserProfile 컴포넌트를 새로 생성하여 사용자 인터페이스 개선

--- src/pages/PostsManagerPage.tsx
+++ src/pages/PostsManagerPage.tsx
@@ -35,6 +35,7 @@ import { Post, Tag } from "../entities/post/model"
 import { User } from "../entities/user/model"
 import { commentQueries } from "../entities/comment/api/queries"
 import { CommentItem } from "../entities/comment/model"
+import { UserProfile } from "../entities/user/ui"
 
 const PostsManager = () => {
   const queryClient = useQueryClient()
@@ -560,31 +561,7 @@ const PostsManager = () => {
           <DialogHeader>
             <DialogTitle>사용자 정보</DialogTitle>
           </DialogHeader>
-          <div className="space-y-4">
-            <img src={selectedUser?.image} alt={selectedUser?.username} className="w-24 h-24 rounded-full mx-auto" />
-            <h3 className="text-xl font-semibold text-center">{selectedUser?.username}</h3>
-            <div className="space-y-2">
-              <p>
-                <strong>이름:</strong> {selectedUser?.firstName} {selectedUser?.lastName}
-              </p>
-              <p>
-                <strong>나이:</strong> {selectedUser?.age}
-              </p>
-              <p>
-                <strong>이메일:</strong> {selectedUser?.email}
-              </p>
-              <p>
-                <strong>전화번호:</strong> {selectedUser?.phone}
-              </p>
-              <p>
-                <strong>주소:</strong> {selectedUser?.address?.address}, {selectedUser?.address?.city},{" "}
-                {selectedUser?.address?.state}
-              </p>
-              <p>
-                <strong>직장:</strong> {selectedUser?.company?.name} - {selectedUser?.company?.title}
-              </p>
-            </div>
-          </div>
+          <UserProfile user={selectedUser} />
         </DialogContent>
       </Dialog>
     </Card>
