f1c94593bbe5278cc04d3b71eed92f4a21b4c964
tksemf7410 <tksemf7410@gmail.com>
2025-08-12 19:18:33 +0900

    chore: Footer, Header 컴포넌트 layout/widgets로 이동

--- src/app/layout/BaseLayout.tsx
+++ src/app/layout/BaseLayout.tsx
@@ -1,5 +1,5 @@
-import Header from "../../components/Header.tsx"
-import Footer from "../../components/Footer.tsx"
+import Header from "../../widgets/layout/ui/Header.tsx"
+import Footer from "../../widgets/layout/ui/Footer.tsx"
 import PostsManagerPage from "../../pages/PostsManagerPage.tsx"
 
 const App = () => {
