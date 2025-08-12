faa2bc1bb7ddf6eb73b3edb6e57bec3fcbbd7722
ChangJun Park <40492343+devchangjun@users.noreply.github.com>
2025-08-13 03:56:14 +0900

    feat: Wrap App component with AppProviders for improved context management

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { BrowserRouter as Router } from "react-router-dom"
+import { AppProviders } from "@/app/providers"
 import { Header, Footer } from "@/app/ui/"
 import PostsManagerPage from "@/pages/PostsManagerPage"
 
 const App = () => {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow container mx-auto px-4 py-8">
-          <PostsManagerPage />
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <AppProviders>
+      <Router>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow container mx-auto px-4 py-8">
+            <PostsManagerPage />
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </AppProviders>
   )
 }
 
--- src/entities/comment/model/hook.ts
+++ src/entities/comment/model/hook.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query"
 import { commentApi } from "../api"
 import { Comment, UpdateComment, CommentFilter } from "./schema"
