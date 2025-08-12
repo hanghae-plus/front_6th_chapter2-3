9e0b4d09eab69d6573d358889210599ebba91b05
JHeeJinDev <zqewe12@gmail.com>
2025-08-12 18:10:51 +0900

    feat: 댓글 타입

---
+++ src/entities/comment/model/types.ts
@@ -0,0 +1,21 @@
+export interface Comment {
+  id: number
+  body: string
+  postId: number
+  likes: number
+  user: {
+    id: number
+    username: string
+  }
+}
+
+export interface CommentsResponse {
+  comments: Comment[]
+  total: number
+}
+
+export interface CreateCommentRequest {
+  body: string
+  postId: number
+  userId: number
+}
