067c8833e33ec03ee7a66ac50bdf06122993a454
JiHoon-0330 <ljihoon0330@gmail.com>
2025-08-12 22:51:07 +0900

    레이아웃 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { BrowserRouter as Router } from 'react-router-dom';
-
-import Footer from '@/components/Footer';
-import Header from '@/components/Header';
-import PostsManagerPage from '@/pages/PostsManagerPage';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Layout } from '@/app/layouts';
+import PostsManagerPage from '@/pages/PostsManagerPage';
 
 const queryClient = new QueryClient();
 
@@ -11,13 +9,9 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <Router>
-        <div className="flex flex-col min-h-screen">
-          <Header />
-          <main className="flex-grow container mx-auto px-4 py-8">
-            <PostsManagerPage />
-          </main>
-          <Footer />
-        </div>
+        <Layout>
+          <PostsManagerPage />
+        </Layout>
       </Router>
     </QueryClientProvider>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { StrictMode } from 'react';
-
 import App from '@/App.tsx';
 import '@/index.css';
 import { createRoot } from 'react-dom/client';
