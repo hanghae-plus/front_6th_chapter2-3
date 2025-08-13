088696f0cc77b164bff9c7a711d2610c9535d9e5
eveneul <nebulas9724@gmail.com>
2025-08-14 05:41:55 +0900

    feat: 게시물과 사용자 정보를 가져오는 API 응답 변수명 수정
    - fetchPostsWithAuthors 함수에서 사용자 정보를 가져오는 변수명을 usersData에서 usersResponse로 변경하여 코드의 명확성을 향상
    - 사용자 정보를 찾는 로직에서 변수명 변경에 따른 수정 적용

--- src/features/posts/list-posts/api.ts
+++ src/features/posts/list-posts/api.ts
@@ -4,12 +4,12 @@ import { fetchUserBasic } from '../../../entities/user/api/api';
 import { UserBasic } from '../../../entities/user/model/type';
 
 export async function fetchPostsWithAuthors(limit: number, skip: string) {
-  const [postsData, usersData] = await Promise.all([fetchPosts(limit, skip), fetchUserBasic()]);
+  const [postsData, usersResponse] = await Promise.all([fetchPosts(limit, skip), fetchUserBasic()]);
 
   return {
     posts: postsData.posts.map((post: Posts) => ({
       ...post,
-      author: usersData.find((user: UserBasic) => user.id === post.userId),
+      author: usersResponse.users.find((user: UserBasic) => user.id === post.userId),
     })),
     total: postsData.total,
   };
