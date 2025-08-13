2760961ac1d5d23e46d51928415651e5ca0eba5f
YeongseoYoon <l112xpla@gmail.com>
2025-08-13 23:08:59 +0900

    feat: 댓글 수정 다이얼로그 훅 추가

---
+++ src/features/comment/edit-comment/lib/useEditCommentDialog.tsx
@@ -0,0 +1,18 @@
+import { useOverlay } from "../../../../shared/lib/use-overlay"
+import { EditCommentDialog } from "../ui/EditCommentDialog"
+
+export const useEditCommentDialog = () => {
+  const { open, overlay } = useOverlay()
+
+  const openEdit = (initialBody: string): Promise<string | undefined> =>
+    open<string>(({ isOpen, close }) => (
+      <EditCommentDialog
+        isOpen={isOpen}
+        onClose={() => close(undefined)}
+        initialBody={initialBody}
+        onConfirm={(body) => close(body)}
+      />
+    ))
+
+  return { openEdit, overlay }
+}
