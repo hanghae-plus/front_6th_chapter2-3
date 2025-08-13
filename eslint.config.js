21b40f5f032b5aee34449cd4fe536620b71278a7
chan9yu <dev.cgyeo@gmail.com>
2025-08-13 18:36:31 +0900

    chore: tanstack query 의존성 설치 및 환경구성
    - eslint plugin 추가
    - dev tool 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import js from "@eslint/js"
+import pluginQuery from "@tanstack/eslint-plugin-query"
 import reactHooks from "eslint-plugin-react-hooks"
 import reactRefresh from "eslint-plugin-react-refresh"
 import simpleImportSort from "eslint-plugin-simple-import-sort"
@@ -6,20 +7,26 @@ import globals from "globals"
 import tseslint from "typescript-eslint"
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ["dist", "tests/**/*", "vitest.config.ts"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
+    files: ["src/**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ["./tsconfig.app.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
+      "@tanstack/query": pluginQuery,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
       "simple-import-sort": simpleImportSort,
     },
     rules: {
+      ...pluginQuery.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
       "simple-import-sort/imports": "error",
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import "@/app/styles/index.css"
 
+import { QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { RouterProvider } from "react-router-dom"
 
+import { queryClient } from "@/app/config"
 import { appRouter } from "@/app/router"
 
 const rootElement = document.getElementById("root")
@@ -14,6 +17,9 @@ if (!rootElement) {
 
 createRoot(rootElement).render(
   <StrictMode>
-    <RouterProvider router={appRouter} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={appRouter} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
 )
