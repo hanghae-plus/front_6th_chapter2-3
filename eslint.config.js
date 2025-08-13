87a6364e11738b40b76a0b62082fa79553730e5e
kyheroumtech <jujini31@gmail.com>
2025-08-13 17:31:22 +0900

    style: 코드 포맷팅 수정

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,49 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import pluginJsxA11y from 'eslint-plugin-jsx-a11y';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    ignores: ['dist', 'node_modules', 'coverage'],
+  },
+  // Global recommended rules
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  // React specific configuration
+  {
     files: ['**/*.{ts,tsx}'],
+    ...pluginReactConfig,
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ...pluginReactConfig.languageOptions,
+      globals: {
+        ...globals.browser,
+      },
     },
     plugins: {
+      react: pluginReact,
+      'jsx-a11y': pluginJsxA11y,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
+      ...pluginJsxA11y.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
     },
   },
-)
+
+  // Prettier config to disable conflicting rules
+  prettierConfig
+);
