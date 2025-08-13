f433c68b11a0cd6a1b3bf689d7f97650a91b4b31
YeongseoYoon <l112xpla@gmail.com>
2025-08-13 22:15:40 +0900

    feat: 오버레이 기능을 위한 useOverlay 훅 추가

---
+++ src/shared/lib/use-overlay.tsx
@@ -0,0 +1,29 @@
+import { ReactNode, useCallback, useMemo, useState } from "react"
+
+export interface OverlayProps<T = unknown> {
+  isOpen: boolean
+  close: (result?: T) => void
+}
+
+export function useOverlay() {
+  const [overlayElement, setOverlayElement] = useState<ReactNode | null>(null)
+
+  const open = useCallback(<T,>(render: (props: OverlayProps<T>) => ReactNode): Promise<T | undefined> => {
+    return new Promise<T | undefined>((resolve) => {
+      const close = (result?: T) => {
+        setOverlayElement(null)
+        resolve(result)
+      }
+
+      setOverlayElement(render({ isOpen: true, close }))
+    })
+  }, [])
+
+  return useMemo(
+    () => ({
+      open,
+      overlay: overlayElement,
+    }),
+    [open, overlayElement],
+  )
+}
