db8868a5bbfd9cef9ef137618442feee2d6dd4b0
YeongseoYoon <l112xpla@gmail.com>
2025-08-13 23:09:04 +0900

    feat: 댓글 수정 기능을 위한 useEditComment 훅 추가

---
+++ src/features/comment/edit-comment/lib/useEditComment.ts
@@ -0,0 +1,17 @@
+import { useMutation } from "@tanstack/react-query"
+import { commentMutations } from "../../../../entities/comment/api/mutations"
+import type { CommentItem } from "../../../../entities/comment/model"
+import { useEditCommentDialog } from "./useEditCommentDialog"
+
+export const useEditComment = () => {
+  const updateCommentMutation = useMutation(commentMutations.updateMutation())
+  const { openEdit, overlay } = useEditCommentDialog()
+
+  const updateComment = async (comment: CommentItem) => {
+    const nextBody = await openEdit(comment.body)
+    if (nextBody == null) return
+    updateCommentMutation.mutate({ id: comment.id, postId: comment.postId, body: nextBody })
+  }
+
+  return { updateComment, overlay, isPending: updateCommentMutation.isPending }
+}
