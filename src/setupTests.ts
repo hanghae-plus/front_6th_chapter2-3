be7a8cc819bb6e093ec0f6143d20697223244e14
YeongseoYoon <l112xpla@gmail.com>
2025-08-15 05:51:19 +0900

    refacotr: setupTests로 이름 수정

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom"
 import { beforeAll, afterEach, afterAll } from "vitest"
-import { server } from "../mocks/server"
+import { server } from "@/mocks/server"
 
 beforeAll(() => server.listen())
 
