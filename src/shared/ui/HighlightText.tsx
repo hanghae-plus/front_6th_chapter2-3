8b173606f16a2d3958f43e9f28a10035793bcb4a
Amelia-Shin <gmldnjs0402@naver.com>
2025-08-14 19:46:29 +0900

    chore: 변수명 변경

--- src/shared/ui/HighlightText.tsx
+++ src/shared/ui/HighlightText.tsx
@@ -1,5 +1,4 @@
-// 하이라이트 함수 추가
-export const highlightText = (text: string, highlight: string) => {
+export const HighlightText = ({ text, highlight }: { text: string; highlight: string }) => {
   if (!text) return null
   if (!highlight.trim()) {
     return <span>{text}</span>
