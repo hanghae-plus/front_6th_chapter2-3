9282e2fb9d05c7214f9d9b0cf17c36b28ca10bd9
YeongseoYoon <l112xpla@gmail.com>
2025-08-14 23:39:24 +0900

    refactor: usePostsBrowseParams 훅에서 페이지네이션 및 추가 파라미터 설정 로직 개선

--- src/features/post/browse-posts/model/usePostsParams.ts
+++ src/features/post/browse-posts/model/usePostsParams.ts
@@ -23,12 +23,20 @@ export const usePostsBrowseParams = () => {
   const setParams = (newParams: Partial<PostsBrowseParams>) => {
     const { sortBy, tag, ...paginationFields } = newParams
 
-    if (Object.keys(paginationFields).length > 0) {
-      setPaginationParams(paginationFields)
+    const definedPagination = Object.fromEntries(
+      Object.entries(paginationFields).filter(([, v]) => v !== undefined),
+    ) as Partial<Pick<PostsBrowseParams, "skip" | "limit" | "searchQuery" | "order">>
+
+    if (Object.keys(definedPagination).length > 0) {
+      setPaginationParams(definedPagination)
     }
 
-    if (sortBy !== undefined || tag !== undefined) {
-      setAdditionalParams({ sortBy, tag })
+    const additionalParams: { sortBy?: string | null; tag?: string | null } = {}
+    if (sortBy !== undefined) additionalParams.sortBy = sortBy === "" ? null : sortBy
+    if (tag !== undefined) additionalParams.tag = tag === "" ? null : tag
+
+    if (Object.keys(additionalParams).length > 0) {
+      setAdditionalParams(additionalParams)
     }
   }
 
