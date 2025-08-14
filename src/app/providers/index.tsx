1dd445a8b3de9fcdbc3e18ab3f6f3b3d87a63c28
ChangJun Park <40492343+devchangjun@users.noreply.github.com>
2025-08-15 04:34:08 +0900

    refactor: Remove StoreProvider and simplify AppProviders to only use QueryProvider

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,15 +1,10 @@
 import { ReactNode } from "react"
 import { QueryProvider } from "@/app/providers/QueryProvider"
-import { StoreProvider } from "@/app/providers/StoreProvider"
 
 interface AppProvidersProps {
   children: ReactNode
 }
 
 export const AppProviders = ({ children }: AppProvidersProps) => {
-  return (
-    <QueryProvider>
-      <StoreProvider>{children}</StoreProvider>
-    </QueryProvider>
-  )
+  return <QueryProvider>{children}</QueryProvider>
 }
