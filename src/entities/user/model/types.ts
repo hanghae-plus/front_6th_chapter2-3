e6f0ab72378737b3e5b7536d221c9ef2175487cb
q1Lim <cochlea00@gmail.com>
2025-08-14 22:59:51 +0900

    feat: UserModal 분리

--- src/entities/user/model/types.ts
+++ src/entities/user/model/types.ts
@@ -1,7 +1,19 @@
 
-// TODO User Interface 추가하기
 export interface User {
   id: number
   username: string
   image?: string | null
+}
+// TODO UserModal 부분
+export interface UserWithInfo extends User {
+  firstName: string
+  lastName: string
+  age: number
+  email: string
+  phone: string
+  address: {
+    address: string
+    city: string
+    state: string
+  }
 }
--- src/pages/PostsManagerPage.tsx
+++ src/pages/PostsManagerPage.tsx
@@ -11,13 +11,12 @@ import {
   DialogContent,
   DialogHeader,
   DialogTitle,
-  Input,
   Textarea,
 } from "../shared/ui"
 import {highlightText} from "../shared/lib/text-utils"
 import { getPosts } from "../entities/post/api/api.ts"
 import { Post } from "../entities/post/model/types.ts"
-import { PostsTable, Pagination, PostsFilter, PostForm } from "../widgets/"
+import { PostsTable, Pagination, PostsFilter, PostForm, UserModal } from "../widgets/"
 
 const PostsManager = () => {
   const navigate = useNavigate()
@@ -504,38 +503,11 @@ const PostsManager = () => {
       </Dialog>
 
       {/* 사용자 모달 */}
-      <Dialog open={showUserModal} onOpenChange={setShowUserModal}>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>사용자 정보</DialogTitle>
-          </DialogHeader>
-          <div className="space-y-4">
-            <img src={selectedUser?.image} alt={selectedUser?.username} className="w-24 h-24 rounded-full mx-auto" />
-            <h3 className="text-xl font-semibold text-center">{selectedUser?.username}</h3>
-            <div className="space-y-2">
-              <p>
-                <strong>이름:</strong> {selectedUser?.firstName} {selectedUser?.lastName}
-              </p>
-              <p>
-                <strong>나이:</strong> {selectedUser?.age}
-              </p>
-              <p>
-                <strong>이메일:</strong> {selectedUser?.email}
-              </p>
-              <p>
-                <strong>전화번호:</strong> {selectedUser?.phone}
-              </p>
-              <p>
-                <strong>주소:</strong> {selectedUser?.address?.address}, {selectedUser?.address?.city},{" "}
-                {selectedUser?.address?.state}
-              </p>
-              <p>
-                <strong>직장:</strong> {selectedUser?.company?.name} - {selectedUser?.company?.title}
-              </p>
-            </div>
-          </div>
-        </DialogContent>
-      </Dialog>
+      <UserModal
+        open={showUserModal}
+        onChangeOpen={setShowUserModal}
+        user={selectedUser}
+      />
     </Card>
   )
 }
