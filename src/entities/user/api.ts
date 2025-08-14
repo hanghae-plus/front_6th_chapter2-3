a5775d6ba4e060a0842d65be817ac9dd47bc1a6a
Legitgoons <cksxkr5193@naver.com>
2025-08-15 04:38:34 +0900

    feat: IFetchUsersResponse type 추가

--- src/entities/user/api.ts
+++ src/entities/user/api.ts
@@ -1,13 +1,23 @@
 import { http } from '@shared/lib/http-client'
+import { IUser } from '@shared/model'
+
+interface IFetchUsersResponse {
+  limit: number
+  skip: number
+  total: number
+  users: IUser[]
+}
 
 /**
  * 사용자 목록 조회
  * @param limit - 한 페이지에 보여지는 사용자 수
  * @returns 사용자 목록
  */
-export async function fetchUsers({ limit }: { limit: number }) {
+export async function fetchUsers({ limit }: { limit: number }): Promise<IFetchUsersResponse> {
   const searchParams = { limit, select: 'username,image' }
-  const response = await http.get(`/users`, { params: searchParams })
+  const response = await http.get<IFetchUsersResponse>(`/users`, {
+    params: searchParams,
+  })
   return response
 }
 
