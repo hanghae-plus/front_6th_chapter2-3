6a8d8e2fc15a10c126dc3dc4ab042fa45afe638e
areumH <dkfmal0109@gmail.com>
2025-08-15 01:21:27 +0900

    chore: 배포 환경 설정

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/front_6th_chapter2-3/',
   test: {
     globals: true,
     environment: 'jsdom',
