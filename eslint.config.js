f133710660096fdd34a25d83e347d480b5b24c1d
chan9yu <dev.cgyeo@gmail.com>
2025-08-14 19:50:20 +0900

    chore: no-namespace rules off

--- eslint.config.js
+++ eslint.config.js
@@ -31,6 +31,7 @@ export default tseslint.config(
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
+      "@typescript-eslint/no-namespace": "off",
     },
   },
 )
