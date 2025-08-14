bdbaaadae097f4bb6ccf7641a9c3436c01abfe71
Angie Lee <angielxx94@gmail.com>
2025-08-14 15:13:01 +0900

    chore: tanstack devtools 설정

--- src/1_app/App.tsx
+++ src/1_app/App.tsx
@@ -17,6 +17,7 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { PostsManagerPage } from '@/pages';
 import { Footer, Header } from '@/widgets';
@@ -26,6 +27,7 @@ const queryClient = new QueryClient();
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={true} />
       <Router>
         <div className='flex flex-col min-h-screen'>
           <Header />
