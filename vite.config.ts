06007225505e855a651af3e44f22c669006c7193
JangRuBin2 <wkdfnqls2465@gmail.com>
2025-08-15 08:53:18 +0900

    chore: 배포설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
-import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import path from "path"
+import { defineConfig } from "vite"
+const base = process.env.NODE_ENV === "production" ? "/front_6th_chapter2-3/" : ""
+const entryFileName = "index.html"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base,
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, entryFileName),
+    },
+  },
   server: {
     proxy: {
       "/api": {
