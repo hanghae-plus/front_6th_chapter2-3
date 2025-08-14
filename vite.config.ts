caea17567d25cb14a2b58ec8e82ed3ac3b276394
eveneul <nebulas9724@gmail.com>
2025-08-15 04:37:40 +0900

    feat: Vite 설정 및 스크립트 추가
    - package.json에 빌드 및 배포 스크립트 추가
    - Vite 설정 파일에서 개발 환경에 따른 API 프록시 설정 및 빌드 옵션 추가
    - 소스 맵 비활성화 및 빌드 아웃풋 디렉토리 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,43 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import path from 'path';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        // target: 'https://jsonplaceholder.typicode.com',
-        target: "https://dummyjson.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig(({ command }) => {
+  // 개발 환경인지 확인
+  const isDev = command === 'serve';
+
+  return {
+    plugins: [react()],
+    base: '/front_6th_chapter2-3/',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'), // 별칭 적용 안했으면 빼도됨
       },
     },
-  },
-})
+    build: {
+      outDir: 'dist',
+      sourcemap: false,
+      minify: 'esbuild',
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ['react', 'react-dom'],
+            query: ['@tanstack/react-query'],
+          },
+        },
+      },
+    },
+    server: {
+      proxy: isDev
+        ? {
+            '/api': {
+              target: 'https://dummyjson.com',
+              changeOrigin: true,
+              rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+          }
+        : undefined,
+    },
+  };
+});
