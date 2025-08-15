eb2445fe0c2869796af6430865fd3426dbdc6c92
이유진 <elli.stdev@gmail.com>
2025-08-15 14:31:26 +0900

    ci: gh-pages 배포 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/front_6th_chapter2-3/',
   server: {
     proxy: {
       "/api": {
