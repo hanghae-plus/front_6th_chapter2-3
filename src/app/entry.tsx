61aa90bdf9f755cf9f192086bae9c14837cebb1d
tksemf7410 <tksemf7410@gmail.com>
2025-08-15 10:31:25 +0900

    chore: tanstack query devtools 설정

--- src/app/entry.tsx
+++ src/app/entry.tsx
@@ -4,12 +4,14 @@ import "./entry.css";
 import App from "./layout/BaseLayout.tsx";
 import { BrowserRouter as Router } from "react-router-dom";
 import { QueryProvider } from "./provider/QueryClientProvider";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
       <QueryProvider>
         <App />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryProvider>
     </Router>
   </StrictMode>,
