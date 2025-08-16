a5650f85b97d5b5fd6cae184d66a1edafca02df0
ChangJun Park <40492343+devchangjun@users.noreply.github.com>
2025-08-16 14:09:53 +0900

    chore: api 타입 추가

--- src/entities/comment/api/index.ts
+++ src/entities/comment/api/index.ts
@@ -2,23 +2,25 @@ import { HttpClient } from "@/shared/api/http"
 import { Comment, CommentPaginatedResponse, CreateComment } from "../model"
 
 // 댓글 목록 조회
-export const getCommentsByPost = (postId: number) => {
+export const getCommentsByPost = (postId: number): Promise<CommentPaginatedResponse> => {
   const url = `/comments/post/${postId}`
   return HttpClient.get<CommentPaginatedResponse>(url)
 }
 
 // 댓글 상세보기
-export const getComment = (id: number) => HttpClient.get<Comment>(`/comments/${id}`)
+export const getComment = (id: number): Promise<Comment> => HttpClient.get<Comment>(`/comments/${id}`)
 
 // 댓글 생성
-export const createComment = (comment: CreateComment) => HttpClient.post<Comment>("/comments/add", comment)
+export const createComment = (comment: CreateComment): Promise<Comment> =>
+  HttpClient.post<Comment>("/comments/add", comment)
 
 // 댓글 수정
-export const updateComment = (id: number, updates: { body?: string }) =>
+export const updateComment = (id: number, updates: { body?: string }): Promise<Comment> =>
   HttpClient.put<Comment>(`/comments/${id}`, updates)
 
 // 댓글 삭제
-export const deleteComment = (id: number) => HttpClient.delete(`/comments/${id}`)
+export const deleteComment = (id: number): Promise<Comment> => HttpClient.delete(`/comments/${id}`)
 
 // 좋아요
-export const likeComment = (id: number, likes: number) => HttpClient.patch<Comment>(`/comments/${id}`, { likes })
+export const likeComment = (id: number, likes: number): Promise<Comment> =>
+  HttpClient.patch<Comment>(`/comments/${id}`, { likes })
--- src/entities/post/api/index.ts
+++ src/entities/post/api/index.ts
@@ -3,7 +3,7 @@ import { buildApiQueryParams, BaseQueryParams } from "@/shared/lib"
 import { CreatePost, Post, PostPaginatedResponse, UpdatePost } from "../model"
 
 // 전체 게시글 조회
-export const getPosts = (filters: BaseQueryParams = {}) => {
+export const getPosts = (filters: BaseQueryParams = {}): Promise<PostPaginatedResponse> => {
   const queryString = buildApiQueryParams(filters)
   const url = `/posts${queryString ? `?${queryString}` : ""}`
   console.log("[DEBUG] url", url)
@@ -11,21 +11,24 @@ export const getPosts = (filters: BaseQueryParams = {}) => {
 }
 
 // 게시물 검색어 조회
-export const getPostBySearch = (searchQuery: string, baseQueryParams: BaseQueryParams = {}) => {
+export const getPostBySearch = (
+  searchQuery: string,
+  baseQueryParams: BaseQueryParams = {},
+): Promise<PostPaginatedResponse> => {
   const queryString = buildApiQueryParams(baseQueryParams)
   const url = `/posts/search?q=${searchQuery}${queryString ? `&${queryString}` : ""}`
   return HttpClient.get<PostPaginatedResponse>(url)
 }
 
 // 게시물 태그별 조회
-export const getPostByTag = (tag: string, filters: BaseQueryParams = {}) => {
+export const getPostByTag = (tag: string, filters: BaseQueryParams = {}): Promise<PostPaginatedResponse> => {
   const queryString = buildApiQueryParams(filters)
   const url = `/posts/tag/${tag}${queryString ? `?${queryString}` : ""}`
   return HttpClient.get<PostPaginatedResponse>(url)
 }
 
 // 게시물 상세 조회
-export const getPost = (id: number) => HttpClient.get<Post>(`/posts/${id}`)
+export const getPost = (id: number): Promise<Post> => HttpClient.get<Post>(`/posts/${id}`)
 
 // 게시물 생성
 export const createPost = async (data: CreatePost): Promise<Post> => {
