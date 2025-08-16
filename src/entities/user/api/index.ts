7d31f3884d1160ee7d2e57e96e455077a761b48a
ChangJun Park <40492343+devchangjun@users.noreply.github.com>
2025-08-16 14:15:14 +0900

    chore: 불필요한 파일 삭제

--- src/entities/user/api/index.ts
+++ src/entities/user/api/index.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@/shared/api/http"
 import { User, UserPaginatedResponse } from "../model"
 
-// 사용자 목록 조회 (간단한 정보만)
+// 사용자 목록 조회
 export const getUsers = async (): Promise<UserPaginatedResponse> => {
   return HttpClient.get<UserPaginatedResponse>("/users?limit=0&select=username,image")
 }
--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,2 +1,1 @@
-export { useQueryParams } from "./useQueryParams"
 export { useBaseQueryParams } from "./useBaseQueryParams"
